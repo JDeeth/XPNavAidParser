fe227173cc53296daa3aad851d0b886959596c21
Jack Deeth <simulationelectronics@gmail.com>
2016-10-30 16:41:44 +0000

    Add coord class to replace vec2 ultimately

--- parseutil.h
+++ parseutil.h
@@ -1,12 +1,59 @@
 #pragma once
 
 #include <algorithm>
+#include <cmath>
 #include <iterator>
 #include <sstream>
 #include <string>
 #include <vector>
 
+class angle {
+protected:
+  double m_deg;
+
+public:
+  double deg() const { return m_deg; }
+  double rad() const { return m_deg * M_PI / 180.; }
+  void set_deg(double degrees) { m_deg = degrees; }
+  void set_rad(double rad) { m_deg = rad * 180 / M_PI; }
+  angle() = default;
+  angle(double degrees) : m_deg{degrees} {}
+};
+
+//class latitude : public angle {
+//public:
+//  angle colat() { return angle(90. - m_deg); }
+//  latitude() = default;
+//  latitude(double degrees) : angle{degrees} {}
+//};
+
+struct coord {
+  angle lat;
+  angle lon;
+  angle colat() { return angle(90. - lat.deg()); }
+  coord() = default;
+  coord(double lat_deg, double lon_deg) : lat{lat_deg}, lon{lon_deg} {}
+};
+
+std::ostream &operator<<(std::ostream &out, const coord &c) {
+  return out << c.lat.deg() << "," << c.lon.deg();
+}
+
+//
+
 using vec2 = std::pair<double, double>;
+
+vec2 radFromDeg(vec2 degrees) {
+  degrees.first *= M_PI / 180.;
+  degrees.second *= M_PI / 180.;
+  return degrees;
+}
+
+vec2 degFromRad(vec2 radians) {
+  radians.first *= 180. / M_PI;
+  radians.second *= 180. / M_PI;
+  return radians;
+}
 
 std::ostream &operator<<(std::ostream &out, const vec2 &v) {
   return out << v.first << "," << v.second;
