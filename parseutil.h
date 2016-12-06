e4b2d0b20daaa421594ec16922464c850caee7b4
Jack Deeth <simulationelectronics@gmail.com>
2016-12-06 05:23:29 +0000

    Rebuild angle as a UDL; deprecate Vec2

--- parseutil.h
+++ parseutil.h
@@ -8,47 +8,77 @@
 #include <vector>
 
 class angle {
-protected:
-  double m_deg;
+private:
+  double m_rad;
 
 public:
-  double deg() const { return m_deg; }
-  double rad() const { return m_deg * M_PI / 180.; }
-  void set_deg(double degrees) { m_deg = degrees; }
-  void set_rad(double rad) { m_deg = rad * 180 / M_PI; }
   angle() = default;
-  angle(double degrees) : m_deg{degrees} {}
+  explicit constexpr angle(long double radians)
+      : m_rad{static_cast<float>(radians)} {}
+
+  angle &operator+=(const angle &rhs) {
+    m_rad += rhs.m_rad;
+    return *this;
+  }
+  angle &operator-=(const angle &rhs) {
+    m_rad -= rhs.m_rad;
+    return *this;
+  }
+
+  operator double() const { return m_rad; }
+
+  void set_rad(double radians) { m_rad = radians; }
+  void set_deg(double degrees) { m_rad = degrees * M_PI / 180.; }
+
+  double deg() const { return m_rad * 180 / M_PI; }
 };
+
+inline angle operator+(angle lhs, const angle &rhs) {
+  lhs += rhs;
+  return lhs;
+}
+inline angle operator-(angle lhs, const angle &rhs) {
+  lhs -= rhs;
+  return lhs;
+}
+
+constexpr angle operator"" _deg(long double degrees) {
+  return angle(degrees * M_PI / 180.);
+}
+
+constexpr angle operator"" _rad(long double radians) { return angle(radians); }
 
 struct coord {
   angle lat;
   angle lon;
-  angle colat() { return angle(90. - lat.deg()); }
+  angle colat() { return angle(90._deg - lat); }
   coord() = default;
-  coord(double lat_deg, double lon_deg) : lat{lat_deg}, lon{lon_deg} {}
+  coord(angle lat, angle lon) : lat(lat), lon(lon) {}
+  coord(double lat_deg, double lon_deg) {
+    lat.set_deg(lat_deg);
+    lon.set_deg(lon_deg);
+  }
 };
 
 std::ostream &operator<<(std::ostream &out, const coord &c) {
   return out << c.lat.deg() << "," << c.lon.deg();
 }
 
-//
+using vec2[[deprecated]] = std::pair<double, double>;
 
-using vec2 = std::pair<double, double>;
-
-vec2 radFromDeg(vec2 degrees) {
+[[deprecated]] vec2 radFromDeg(vec2 degrees) {
   degrees.first *= M_PI / 180.;
   degrees.second *= M_PI / 180.;
   return degrees;
 }
 
-vec2 degFromRad(vec2 radians) {
+[[deprecated]] vec2 degFromRad(vec2 radians) {
   radians.first *= 180. / M_PI;
   radians.second *= 180. / M_PI;
   return radians;
 }
 
-std::ostream &operator<<(std::ostream &out, const vec2 &v) {
+[[deprecated]] std::ostream &operator<<(std::ostream &out, const vec2 &v) {
   return out << v.first << "," << v.second;
 }
 
